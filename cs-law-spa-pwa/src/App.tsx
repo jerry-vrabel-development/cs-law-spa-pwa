b591549d491c8759a5ffb5ad95c665659a0faf3e
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2024-04-11 12:01:52 -0500

    config OG tags for react-helmt-async

--- cs-law-spa-pwa/src/App.tsx
+++ cs-law-spa-pwa/src/App.tsx
@@ -1,4 +1,4 @@
-import { HelmetProvider } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero'
 import LawServices from './components/Law-Services/LawServices';
@@ -13,6 +13,25 @@ function App() {
   return (
     <>
       <HelmetProvider>
+        <Helmet>
+          { /* Standard metadata tags */}
+          <title>Conrad Szewczyk & Associates</title>
+          <meta name='description' content='Conrad Szewczyk & Associates, unwavering dedication to justice and a commitment to excellence in the legal profession.' />
+          { /* End standard metadata tags */}
+          { /* Facebook tags */}
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content='Conrad Szewczyk & Associates' />
+          <meta property="og:description" content='Conrad Szewczyk & Associates, unwavering dedication to justice and a commitment to excellence in the legal profession.' />
+          <meta property="og:image" content="/csa-logo.png" />
+          <meta property="og:url" content="https://cs-law-spa-pwa.vercel.app/" />
+          { /* End Facebook tags */}
+          { /* Twitter tags */}
+          <meta name="twitter:creator" content="" />
+          <meta name="twitter:card" content="" />
+          <meta name="twitter:title" content="" />
+          <meta name="twitter:description" content="" />
+          { /* End Twitter tags */}
+        </Helmet>
         <Header />
         <Hero />
         <section>
