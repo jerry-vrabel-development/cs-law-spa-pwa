ec760e8431ae111afc93ee4362ce86ef13632aeb
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2024-05-01 17:18:33 -0500

    style and content changes

--- cs-law-spa-pwa/src/components/Calendly/Calendly.tsx
+++ cs-law-spa-pwa/src/components/Calendly/Calendly.tsx
@@ -7,17 +7,19 @@ const Calendly: React.FC = () => {
   return (
     <section id="calendly" className="calendly-section">
       <h2><GrSchedule aria-label="Schedule a Consultation" className="calendly-section-icon" />Schedule a Consultation</h2>
-      <div className="inline-widget">
-        <InlineWidget
-          pageSettings={{
-          backgroundColor: '#003366',
-          hideEventTypeDetails: false,
-          hideLandingPageDetails: false,
-          primaryColor: '#ffffff',
-          textColor: 'ffffff'
-          }}
-          url="https://calendly.com/jerry-vrabel-development" />
-      </div>
+      <div className='calendly-widget' >
+        <div className="inline-widget">
+          <InlineWidget
+            pageSettings={{
+            backgroundColor: '#003366',
+            hideEventTypeDetails: false,
+            hideLandingPageDetails: false,
+            primaryColor: '#ffffff',
+            textColor: '#ffffff',
+            }}
+            url="https://calendly.com/jerry-vrabel-development" />
+        </div>
+        </div>
     </section>
   )
 };
