c1fbb48e3931aff3ffd660694c8645801e63011c
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2024-05-02 13:07:28 -0500

    multi-lang *WIP

--- cs-law-spa-pwa/src/App.tsx
+++ cs-law-spa-pwa/src/App.tsx
@@ -1,3 +1,4 @@
+import { I18nextProvider } from 'react-i18next';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero'
 import LawServices from './components/Law-Services/LawServices';
@@ -6,21 +7,24 @@ import Calendly from './components/Calendly/Calendly';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import './App.css'
+import i18n from './components/i18n';
 
 function App() {
 
   return (
-    <div role="main">
-      <Header />
-      <Hero />
-      <section>
-        <LawServices />
-        <About />
-        <Calendly />
-        <Contact />
-      </section>
+    <I18nextProvider i18n={i18n}>
+      <div role="main">
+        <Header />
+        <Hero />
+        <section>
+          <LawServices />
+          <About />
+          <Calendly />
+          <Contact />
+        </section>
         <Footer />
-    </div>
+      </div>
+      </I18nextProvider>
   )
 }
 
